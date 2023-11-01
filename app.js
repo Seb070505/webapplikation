7d4c66ee7550dd59dd159214b2743707e0e50e20
Seb070505 <sebastianmeinertz0705@gmail.com>
2023-11-01 09:49:53 +0100

    lmao

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dorito = loadImage('assets/dorito.png')
 
 function setup() {
     canvas = createCanvas(300, 550, 'beholder');
-    textSize(24);
+    textSize(10);
     background(255, 255, 255);
     imageMode(CENTER);
     // giver canvas border på 2 pixel, 
@@ -88,7 +88,7 @@ function draw() {
 
 text('Hoppet: ' + str(hoppet), 50, height-100);
 text('Løbet: ' + str(løbet),50, height-50);
-text('Bevæg dig tyksak: ' + str(stille),50,height-150)
+text('Bevæg dig tyksak: ' + str(stille),10,height-150)
 }
 
 function deviceMoved(){
