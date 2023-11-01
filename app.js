3bec2053d6f07a02a97eeeb29110e30623266c2a
Seb070505 <sebastianmeinertz0705@gmail.com>
2023-11-01 08:46:11 +0100

    i want to be ninja, i wanna chop chop chop

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let hoppet = 0;
 function setup() {
     canvas = createCanvas(300, 550, 'beholder');
     textSize(24);
+    background(255,0,255);
     // giver canvas border på 2 pixel, 
     // og sørger derefter for at kanten tælles med i width
     canvas.elt.style.border = '5px solid black';
@@ -33,7 +34,6 @@ function setup() {
 }
 
 function draw() {
-    background(255,0,255);
     strokeWeight(10);
     if (firkant === false && trekant === false && circle === true){
         ellipse(pos, pos2, 50);
